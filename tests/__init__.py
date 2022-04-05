63f683782f356d5563e08af6e2f60a2ce145a387
Geoff Low <glow@mdsol.com>
2022-04-05 12:33:51 +0100

    Added routines to help find data/fixture files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,26 @@
 
 import os
 
-def get_data_file_path(filename: str):
+def remove_prefix(filepath, parent_dir):
+    """
+    Remove the parent_dir prefix from the filepath.
+    """
+    if filepath.startswith(parent_dir):
+        filepath = filepath[len(parent_dir):]
+    return filepath
+
+
+def get_data_file_path(filename: str, check_exists: bool = True):
     # strip off ./data prefix if it is there
-    if filename.startswith("./data"):
-        filename = os.path.split(filename)[-1]
     target = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', filename)
+    if check_exists and not os.path.exists(target):
+        raise FileNotFoundError(f"File {target} not found")
+    return target
+
+
+def get_fixture_file_path(filename: str):
+    # strip off ./fixtures prefix if it is there
+    target = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fixtures', filename)
     if not os.path.exists(target):
         raise FileNotFoundError(f"File {target} not found")
     return target
-    
